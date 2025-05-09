89bfaf44c5cdb06895bdc0dbbdec01c279756986
Muhammed Yeter <muhammedyeter4736@gmail.com>
2025-05-09 20:09:48 +0300

    Uploading Project
    uploading random password generator.

---
+++ index.js
@@ -0,0 +1,29 @@
+async function getPasswordFromAPI() {
+    var pWordLength, nums, caps, chars;
+    chars = (document.getElementById("haveChars").checked ? "special&" : "");
+    caps = (document.getElementById("haveCaps").checked ? "uppercase&lowercase" : "lowercase");
+    nums = document.getElementById("haveNums").checked;
+    errorMessage = document.getElementById("error-message");
+    pWordLength = document.getElementById("length").value;
+
+    if (pWordLength.length > 0 && pWordLength > 6 && pWordLength < 100) {
+        await fetch('https://api.genratr.com/?length=' + pWordLength + '&' + caps + '&' + chars + nums)
+            .then((res) => res.json())
+            .then((data) => {
+                document.getElementById("passwordDisplayer").innerText = data.password;
+            });
+    }
+    else {
+        errorMessage.style.display = "block";
+        errorMessage.innerText = "Yanlış bir uzunluk belirlediniz";
+        setTimeout(function () {
+            errorMessage.style.display = "none";
+        }, 2000)
+    }
+}
+
+function copyPassword() {
+    var copyText = document.getElementById("passwordDisplayer");
+    navigator.clipboard.writeText(copyText.innerText);
+    alert("Password has been copied to the clipboard.")
+}
